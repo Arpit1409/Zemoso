f53ff998fa583a4c9fa86e44f3090363dd6f05f2
Arpit1409 <am149.ubt2014@iitr.ac.in>
2018-01-22 21:35:50 +0530

    Problem

---
+++ Problem_3.py
@@ -0,0 +1,13 @@
+ctr = 0
+temp = s[0]
+ans = s[0]
+for i in range(len(s) - 1):
+    if s[i + 1] >= s[i]:
+        temp += s[i + 1]
+        if len(temp) > ctr:
+            ctr = len(temp)
+            ans = temp
+    else:
+        temp=s[i + 1]
+    i += 1
+print (ans)
