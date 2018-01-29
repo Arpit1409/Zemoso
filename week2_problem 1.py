8089e84b2315498ff9dbd1a3c45a0b14e733640c
Arpit1409 <am149.ubt2014@iitr.ac.in>
2018-01-29 09:39:27 +0530

    Problem

--- week2_problem 1.py
+++ week2_problem 1.py
@@ -1,7 +1,7 @@
 #Problem 1
-balance = input("Balance: ")
-monthlyPaymentRate = input("Monthly Payment Rate: ")
-annualInterestRate = input("Annual Interest Rate: ")
+balance = float(input("Balance: "))
+monthlyPaymentRate = float(input("Monthly Payment Rate: "))
+annualInterestRate = float(input("Annual Interest Rate: "))
 for i in range(12):
     balance = balance - (balance * monthlyPaymentRate) + ((balance - (balance * monthlyPaymentRate)) * (annualInterestRate/12))
 print("Remaining balance:", round(balance, 2))
