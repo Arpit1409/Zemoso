68b4a2e81354793eff53f29165393e16df6e16ae
Arpit1409 <am149.ubt2014@iitr.ac.in>
2018-01-29 09:48:39 +0530

    Problem

---
+++ week2_problem 3.py
@@ -0,0 +1,21 @@
+#problem 3
+balance = float(input("Balance: "))
+annualInterestRate = float(input("Annual Interest Rate: "))
+init_balance = balance
+monthlyInterestRate = annualInterestRate/12
+lower = init_balance/12
+upper = (init_balance * (1 + monthlyInterestRate)**12)/12.0
+epsilon = 0.01
+
+while abs(balance) > epsilon:
+    monthlyPaymentRate = (upper + lower)/2
+    balance = init_balance
+    for i in range(12):
+        balance = balance - monthlyPaymentRate + ((balance - monthlyPaymentRate) * monthlyInterestRate)
+    if balance > epsilon:
+        lower = monthlyPaymentRate
+    elif balance < -epsilon:
+        upper = monthlyPaymentRate
+    else:
+        break
+print('Lowest Payment:', round(monthlyPaymentRate, 2))
